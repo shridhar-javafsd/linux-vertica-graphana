b35d3b5d4a6b488d702f65d0d33f129b7082d224
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-09-02 16:57:17 +0530

    final picture

--- vertica-stuff/Script.sql
+++ vertica-stuff/Script.sql
@@ -1,4 +1,14 @@
+RDBMS 
 
+OLTP - MySQL - car 
+OLAP - Vertica - bus 
 
+SELECT version();
+
+-- how many table are there in this database?
+
+SELECT count(*) FROM all_tables; 
+Select count(*) FROM all_tables; 
+select count(*) FROM all_tables; 
 
 
