efd97a4afa5a30a103dee96edd71ad0b28903768
Vamz <dyesmuk@gmail.com>
2026-09-03 08:34:04 +0530

    sql stuff

--- vertica-stuff/Script.sql
+++ vertica-stuff/Script.sql
@@ -7,8 +7,6 @@ SELECT version();
 
 -- how many table are there in this database?
 
-SELECT count(*) FROM all_tables; 
-Select count(*) FROM all_tables; 
+SELECT count(*) FROM eMployees WHERE first_name = 'SAM'; 
+Select count(*) FROM All_tables WHERE table_name = 'SAM'; 
 select count(*) FROM all_tables; 
-
-
